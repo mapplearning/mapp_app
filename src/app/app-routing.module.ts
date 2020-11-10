1fcd3b5384b4f903631fae2dc3636c62fcb97e15
Hailey Shewprasad <haileyshewprasad@gmail.com>
2020-11-10 15:22:50 -0500

    button functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StudentLoginComponent } from './student-login/student-login.component';
+import { TeacherLoginComponent } from './teacher-login/teacher-login.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 const routes: Routes = [
-  { path: 'welcome-page', component: WelcomePageComponent },
   { path: 'student-login', component: StudentLoginComponent },
+  { path: 'teacher-login', component: TeacherLoginComponent }
 ];
 
 @NgModule({
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.css']
 })
-export class WelcomePageComponent implements OnInit {
 
-  constructor() { }
+export class WelcomePageComponent implements OnInit {
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onStudentLoginClick(){
+    this.router.navigate(['/student-login']);
+  }
+  onTeacherLoginClick(){
+    this.router.navigate(['/teacher-login']);
+
   }
 
 }
